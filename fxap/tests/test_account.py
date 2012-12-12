c6b3863ee191c13181c07fe7cda183a4894e32bb
Nick Alexander <nalexander@mozilla.com>
2012-12-11 16:38:27 -0800

    Refactor tests.

--- fxap/tests/test_account.py
+++ fxap/tests/test_account.py
@@ -10,64 +10,61 @@ random.seed(1000)
 class TestAccount(unittest.TestCase):
     def setUp(self):
         self.app = TestApp(main({}))
-        self.email = "%d@bar.com" % random.randint(1000000, 9999999)
+        i = random.randint(1000000, 9999999)
+        self.email = "%d@bar.com" % i
+        self.salt = "salt-%d" % i
+        self.S1 = "S1-%d" % i
+
+    def post(self, uri, json_body, *args, **kw):
+        json_body['email'] = self.email
+        json_body['S1'] = self.S1
+        return self.app.post(uri, json.dumps(json_body), *args, **kw)
+
+    def post_unauth(self, uri, json_body, *args, **kw):
+        return self.app.post(uri, json.dumps(json_body), *args, **kw)
 
     def test_bad_create(self):
-        res = self.app.post('/account/create', status=400)
+        res = self.post('/account/create', {}, status=400)
 
-    def _create(self, email, *args, **kw):
+    def _create(self, *args, **kw):
         body = {}
-        body['email'] = email
-        body['salt'] = 'salt'
-        body['S1'] = 'S1'
+        body['email'] = self.email
+        body['salt'] = self.salt
+        body['S1'] = self.S1
 
-        res = self.app.post('/account/create', json.dumps(body), *args, **kw)
+        res = self.post_unauth('/account/create', body, *args, **kw)
         return res
 
     def test_create(self):
 
-        res = self._create(self.email)
+        res = self._create()
         assert res.json['email'] == self.email
 
     def test_already_used(self):
-        res = self._create(self.email)
+        res = self._create()
         assert res.json['email'] == self.email
 
-        self._create(self.email, status=409)
+        self._create(status=409)
 
     def test_uk_no_acocunt(self):
-        body = {}
-        body['email'] = self.email
-        body['S1'] = 'S1'
-
-        res = self.app.post('/key/uk/get', json.dumps(body), status=401)
+        res = self.post('/key/uk/get', {}, status=401)
 
     def test_get_no_uk(self):
-        res = self._create(self.email)
+        res = self._create()
         assert res.json['email'] == self.email
 
-        body = {}
-        body['email'] = self.email
-        body['S1'] = 'S1'
-
-        res = self.app.post('/key/uk/get', json.dumps(body), status=404)
+        res = self.post('/key/uk/get', {}, status=404)
 
     def test_uk(self):
         KEY = 'uk uk uk'
 
-        res = self._create(self.email)
+        res = self._create()
         assert res.json['email'] == self.email
 
         body = {}
-        body['email'] = self.email
-        body['S1'] = 'S1'
         body['key'] = KEY
 
-        res = self.app.post('/key/uk/put', json.dumps(body))
+        res = self.post('/key/uk/put', body)
 
-        body = {}
-        body['email'] = self.email
-        body['S1'] = 'S1'
-
-        res = self.app.post('/key/uk/get', json.dumps(body))
+        res = self.post('/key/uk/get', {})
         assert res.json['key'] == KEY
